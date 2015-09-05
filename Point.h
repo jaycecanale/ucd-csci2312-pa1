8a9e355e2a8a4ca44c427e1b143aa9db041b6ce7
jaycecanale <jayce.canale@ucdenver.edu>
2015-09-04 21:47:46 -0600

    Update Point.h

--- Point.h
+++ Point.h
@@ -1,5 +1,10 @@
-// A 2-dimensional point class!
+// A 3-dimensional point class!
 // Coordinates are double-precision floating point.
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
 
 #ifndef __point_h
 #define __point_h
@@ -9,11 +14,13 @@ class Point {
 private:
   double x;
   double y;
+  double z;
 
 public:
   // Constructors
   Point();                      // default constructor
-  Point(double x, double y);    // two-argument constructor
+  Point(double x, double y, double z);    // three-argument constructor
+  double distanceTo(Point&);
 
   // Destructor
   ~Point();
@@ -21,10 +28,12 @@ public:
   // Mutator methods
   void setX(double newX);
   void setY(double newY);
+  void setZ(double newZ);
 
   // Accessor methods
   double getX();
   double getY();
+  double getZ();
 };
 
 #endif // __point_h
